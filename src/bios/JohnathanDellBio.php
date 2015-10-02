13a1facf33d79c3e2d622399c0bcdce13401afd9
Johnathan Dell <johnathandell@Johnathans-MacBook-Pro.local>
2015-10-02 14:54:18 +0200

    Added Johnathan Dell Bio

---
+++ src/bios/JohnathanDellBio.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace bios;
+
+
+class JohnathanDellBio extends AbstractBio
+{
+
+    /**
+     * Provides the full name of the person that the bio is about
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Johnathan Dell';
+    }
+
+    /**
+     * Provides a paragraph of test about the person that the bio is about
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return 'Proponent of design philosophies and good development practises.';
+    }
+
+    /**
+     * Should return a publicly accessible URL to an image of the person that the bio is about.
+     *
+     * @return null|string
+     */
+    public function getProfileImageURL()
+    {
+        return 'https://cdn.afrihost.com/images/meet_the_team_new/dev_johnathan_d.jpg';
+    }
+
+
+}
