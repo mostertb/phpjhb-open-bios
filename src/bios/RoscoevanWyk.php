d6b9c841fb9b2107c79668b456cd66b10db3b4a1
Roscoe van Wyk <roscoew@afrihost.com>
2015-10-02 15:24:30 +0200

    New bio for Roscoe van Wyk also formatted according to AirBnB standards

---
+++ src/bios/RoscoevanWyk.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace bios;
+
+
+class RoscoevanWykBio extends AbstractBio
+{
+
+    /**
+     * Provides the full name of the person that the bio is about
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Roscoe van Wyk';
+    }
+
+    /**
+     * Provides a paragraph of test about the person that the bio is about
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return 'Epic lols and boerewors rolls. :D';
+    }
+
+    /**
+     * Should return a publicly accessible URL to an image of the person that the bio is about.
+     *
+     * @return null|string
+     */
+    public function getProfileImageURL()
+    {
+        return 'http://placekitten.com/64/64';
+    }
+
+
+}
